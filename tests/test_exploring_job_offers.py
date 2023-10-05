79997c8c2b65f1822be65bcb817b1240b607a9af
RyuKoBaihu <yamamoto.araujo.matheus@gmail.com>
2023-10-05 17:21:26 -0300

    ec implements

--- tests/test_exploring_job_offers.py
+++ tests/test_exploring_job_offers.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium.webdriver.common.by import By
 from pages.BasePage import BasePage
+from selenium.webdriver.support import expected_conditions as ec
 
 
 @pytest.mark.usefixtures("setup_teardown")
@@ -9,4 +10,6 @@ class TestExploringJobOffers:
     def test_exploring_job_offers(self):
         test = BasePage()
         test.click(By.XPATH, '//*[@id="root"]/div/div/main/div[2]/div/div/p/a')
+        ec.title_is("Our job offers - Natixis GFS")
+        ec.url_changes("https://recrutement.natixis.com/en/our-job-offers?external=false")
         test.search("QA")
